60fca1d3ef34ede9742fa3e87d23c204561fc00f
mhvozdesky <m.hvozdesky@gmail.com>
2022-07-13 14:02:53 +0300

    changed files .env.dev, docker-compose.yml, fatcode/settings.py

--- fatcode/settings.py
+++ fatcode/settings.py
@@ -70,7 +70,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB', 'postgres'),
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.environ.get('DB_HOST', 'db'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
