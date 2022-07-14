99eeec69222738ba05cb5e57f75917cf999610a6
mhvozdesky <m.hvozdesky@gmail.com>
2022-07-14 14:26:10 +0300

    implemented the ability to upload the user's avatar and social logo. Avatars are given unique names. Added comments to models

--- fatcode/settings.py
+++ fatcode/settings.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,6 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
