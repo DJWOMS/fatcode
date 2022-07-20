7e38fd2df5511cec930ffc953ebcd27fddcad79a
mhvozdesky <m.hvozdesky@gmail.com>
2022-07-20 18:54:48 +0300

    Added models Article, Category, Tag

--- src/knowledge/admin.py
+++ src/knowledge/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from src.knowledge.models import Category, Tag, Article
 
-# Register your models here.
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Tag)
--- src/knowledge/models.py
+++ src/knowledge/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from src.profiles.models import FatUser
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(FatUser, on_delete=models.SET_NULL, null=True)
+    view_count = models.IntegerField(default=0)
+    picture = models.ImageField(
+        upload_to='article/picture',
+        null=True,
+        blank=True
+    )
+    category = models.ManyToManyField(Category)
+    tag = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ['date_creation']
+
+    def __str__(self):
+        return self.title
