76700cd42f40a228579fc5995f119e12c86d09a9
Omelchenko Michael <socanime@gmail.com>
2022-09-03 16:35:07 +0300

    Fix url questions

--- src/questions/urls.py
+++ src/questions/urls.py
@@ -7,19 +7,17 @@ urlpatterns = [
     }), name="question-list"),
     path('answer/<int:id>/', views.AnswerView.as_view({
         'put': 'update',
-        'patch': 'partial_update',
         'delete': 'destroy'
     }), name="answer"),
-    path('create_answer/', views.CreateAnswerView.as_view({
+    path('answer/', views.CreateAnswerView.as_view({
         'post': 'create'
     }), name="create-answer"),
     path('<int:id>/', views.QuestionView.as_view({
         'get': 'retrieve',
         'put': 'update',
-        'patch': 'partial_update',
         'delete': 'destroy'
     }), name="question"),
-    path('question_review/', views.CreateQuestionReview.as_view({
+    path('review/', views.CreateQuestionReview.as_view({
         'post': 'create'
     }), name="question-review"),
     path('answer_review/', views.CreateAnswerReview.as_view({
