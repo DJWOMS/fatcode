7b2e42af45adfa5305317be646b83376ee296ce5
Maksim <m.metelev97@gmail.com>
2022-09-05 16:56:51 +0300

    Adding filter by published and changing serializers

--- src/knowledge/serializers.py
+++ src/knowledge/serializers.py
@@ -1,27 +1,57 @@
 from rest_framework import serializers
+
 from src.knowledge import models
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
-        fields = ('id', 'name', 'parent', 'article')
+        fields = ('id', 'name', 'parent')
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
-        fields = ('id', 'name', 'article')
+        fields = ('id', 'name')
         ref_name = 'tag_knowledge'
 
 
 class ListArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Article
-        exclude = ['text']
+        exclude = ['text', 'video_url']
+
+
+class CategoryArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = ['id', 'name']
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.FatUser
+        fields = ['id', 'username']
 
 
 class DetailArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+    category = CategoryArticleSerializer(many=True)
+    tag = TagSerializer(many=True)
+
     class Meta:
         model = models.Article
-        fields = '__all__'
+        fields = [
+            'id',
+            'title',
+            'text',
+            'published',
+            'date_creation',
+            'date_update',
+            'author',
+            'view_count',
+            'picture',
+            'category',
+            'tag',
+            'video_url'
+        ]
