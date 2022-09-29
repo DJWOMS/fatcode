573c06acabab358abd2ef214f598782e60192701
Omelchenko Michael <socanime@gmail.com>
2022-09-29 16:22:22 +0300

    Перенес валидатор изображений

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
-from src.profiles.validators import ImageValidator
+from src.base.validators import ImageValidator
 from src.courses.models import Course
 
 
