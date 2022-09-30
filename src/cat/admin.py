fd020a22a32133e6eb5238659cb0730f81c0b4d0
nikitqa48 <Princen1k@yandex.com>
2022-09-30 13:21:27 +0300

    add admin models

--- src/cat/admin.py
+++ src/cat/admin.py
@@ -5,3 +5,5 @@ from . import models
 admin.site.register(models.Category)
 admin.site.register(models.Product)
 admin.site.register(models.Cat)
+admin.site.register(models.Inventory)
+admin.site.register(models.Item)
