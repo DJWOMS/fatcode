f96e5d3929fdf6255dcb805689e1d0c2836f68c9
Maksim <m.metelev97@gmail.com>
2022-10-04 20:40:09 +0300

    Create cat signal

--- src/cat/signals.py
+++ src/cat/signals.py
@@ -1,9 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
 from .models import Cat, Inventory
+from src.profiles.models import FatUser
+
+
+@receiver(post_save, sender=FatUser)
+def create_cat(sender, instance, created, **kwargs):
+    if created:
+        Cat.objects.create(user=instance)
 
 
 @receiver(post_save, sender=Cat)
-def create_profile(sender, instance, created, **kwargs):
+def create_inventory(sender, instance, created, **kwargs):
     if created:
         Inventory.objects.create(cat=instance)
