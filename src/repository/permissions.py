4b1bc4334f4b46a4a5a63847c01d47fb8e3dbe61
zahar4ik <zakhar_varfolomeev@mail.ru>
2022-10-17 15:25:12 +0300

    refactor

--- src/repository/permissions.py
+++ src/repository/permissions.py
@@ -8,11 +8,8 @@ from src.team.permissions import is_author_of_team_for_project
 class ProjectPermission(BasePermission):
     def has_permission(self, request, view):
         if is_author_of_team_for_project(request):
-            if Project.objects.filter(repository=request.data['repository']):
-                return False
-            else:
-                repository = get_my_repository(request.data['repository'], request.user)
-                if repository:
+            if not Project.objects.filter(repository=request.data['repository']).exists():
+                if get_my_repository(request.data['repository'], request.user):
                     return True
 
 
