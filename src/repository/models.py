0eb00d8faadd4afb76a716c0a3dce7a28b36080a
Maksim <m.metelev97@gmail.com>
2022-10-23 19:14:28 +0300

    Repository optimization and refactoring

--- src/repository/models.py
+++ src/repository/models.py
@@ -1,56 +1,33 @@
 from django.core.validators import FileExtensionValidator
 from django.db import models
+
 from src.profiles.models import FatUser
 from src.base.validators import ImageValidator
 from src.team.models import Team
 
 
 class Category(models.Model):
-    """ Categories by project """
     name = models.CharField(max_length=150)
-    parent = models.ForeignKey(
-        'self',
-        on_delete=models.SET_NULL,
-        related_name="children",
-        blank=True,
-        null=True
-    )
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name="children", blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Toolkit(models.Model):
-    """ Toolkit by project """
     name = models.CharField(max_length=150)
-    parent = models.ForeignKey(
-        'self',
-        on_delete=models.SET_NULL,
-        related_name="children",
-        blank=True,
-        null=True
-    )
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name="children", blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Project(models.Model):
-    """ Model project """
     name = models.CharField(max_length=150)
     description = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(
-        FatUser,
-        on_delete=models.CASCADE,
-        related_name="projects"
-    )
-    category = models.ForeignKey(
-        Category,
-        related_name="projects",
-        on_delete=models.SET_NULL,
-        null=True
-    )
+    user = models.ForeignKey(FatUser, on_delete=models.CASCADE, related_name="projects")
+    category = models.ForeignKey(Category, related_name="projects", on_delete=models.SET_NULL, null=True)
     toolkit = models.ManyToManyField(Toolkit, related_name="projects")
     teams = models.ManyToManyField(Team, related_name='project_teams')
     avatar = models.ImageField(
