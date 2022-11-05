2b2e3dc46fabb21d68ffea46caed16c769b2e486
Maksim <m.metelev97@gmail.com>
2022-11-05 22:20:00 +0300

    CRUD for courses and lessons

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -3,9 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('detail/<int:id>/', views.DetailCourseView.as_view(), name="course-detail"),
-    path('list/', views.ListCourseView.as_view(), name="course-list"),
-    path('lesson/<int:id>/', views.DetailLessonView.as_view(), name="lesson-detail"),
     path('check_work/', views.StudentWorkView.as_view(), name="check-work"),
-    path('help_mentor/', views.HelpUserView.as_view(), name="help-mentor")
+    path('help_mentor/', views.HelpUserView.as_view(), name="help-mentor"),
+
+    path('', views.CourseView.as_view({"get": "list", "post": "create"}), name='courses'),
+    path('<int:pk>/', views.CourseView.as_view(
+        {"get": "retrieve", "delete": "retrieve", "patch": "update"}
+    ), name='course'),
+
+    path('lessons/', views.LessonView.as_view({"get": "list", "post": "create"}), name='lessons'),
+    path('lessons/<int:pk>/', views.LessonView.as_view(
+        {"get": "retrieve", "delete": "retrieve", "patch": "update"}
+    ), name='lesson')
 ]
