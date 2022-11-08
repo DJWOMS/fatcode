60ba4801cc3aec78a01f7568628b4d27d80798dd
zahar4ik <zakhar_varfolomeev@mail.ru>
2022-11-08 08:25:28 +0300

    Revert "forgot to delete print in permission"
    This reverts commit 75aae23b6e6d62464b0bb0892427de288657701e.

--- src/questions/permissions.py
+++ src/questions/permissions.py
@@ -5,6 +5,7 @@ from . import models
 
 class IsNotFollower(BasePermission):
     def has_permission(self, request, view):
+        print(request.data['question'], request.user)
         if models.QuestionFollowers.objects.filter(question=request.data['question'], follower=request.user):
             return False
         return True
