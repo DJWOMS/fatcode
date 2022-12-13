6e4766e0d570baf60cf8f6eb8d1d4b24477fb31d
Omelchenko Michael <socanime@gmail.com>
2022-12-13 19:17:04 +0300

    вернул файлы аватаров

--- src/repository/models.py
+++ src/repository/models.py
@@ -8,7 +8,13 @@ from src.base.validators import ImageValidator
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name="children", blank=True, null=True)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        related_name="children",
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.name
@@ -16,7 +22,13 @@ class Category(models.Model):
 
 class Toolkit(models.Model):
     name = models.CharField(max_length=150)
-    parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name="children", blank=True, null=True)
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        related_name="children",
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
         return self.name
@@ -27,7 +39,12 @@ class Project(models.Model):
     description = models.TextField()
     create_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey('profiles.FatUser', on_delete=models.CASCADE, related_name="projects")
-    category = models.ForeignKey(Category, related_name="projects", on_delete=models.PROTECT, default='1')
+    category = models.ForeignKey(
+        Category,
+        related_name="projects",
+        on_delete=models.PROTECT,
+        default='1'
+    )
     toolkit = models.ManyToManyField(Toolkit, related_name="projects")
     teams = models.ManyToManyField('team.Team', related_name='project_teams')
     avatar = models.ImageField(
@@ -51,7 +68,11 @@ class Project(models.Model):
 
 
 class ProjectMember(models.Model):
-    user = models.ForeignKey('profiles.FatUser', on_delete=models.CASCADE, related_name='project_members')
+    user = models.ForeignKey(
+        'profiles.FatUser',
+        on_delete=models.CASCADE,
+        related_name='project_members'
+    )
     project = models.ForeignKey(Project, related_name="members", on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
 
