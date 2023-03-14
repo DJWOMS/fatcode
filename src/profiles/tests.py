fee4e4b67d8c9016bb7216c90fdda3ecdd33bc31
Omelchenko Michael <socanime@gmail.com>
2023-03-14 12:37:53 +0300

    add bad test

--- src/profiles/tests.py
+++ src/profiles/tests.py
@@ -55,18 +55,18 @@ class ProfileRegTests(APITestCase):
         )
         self.invite.save()
 
-    # def test_create_user(self):
-    #     self.client.post('/api/v1/auth/users/', user_create_data, format='json')
-    #     user = FatUser.objects.get(email='antonenique@example.com')
-    #     self.assertEqual(user.username, 'anton')
-    #     self.assertEqual(user.email, 'antonenique@example.com')
+    def test_create_user(self):
+        self.client.post('/api/v1/auth/users/', user_create_data, format='json')
+        user = FatUser.objects.get(email='antonenique@example.com')
+        self.assertEqual(user.username, 'anton')
+        self.assertEqual(user.email, 'antonenique@example.com')
 
-    # def test_create_user_error_pass(self):
-    #     data = user_create_data.copy()
-    #     data['re_password'] = 'V97tn7M4ru'
-    #     request = self.client.post('/api/v1/auth/users/', data, format='json')
-    #     self.assertEqual(request.status_code, 400)
-    #     self.assertEqual(request.data['non_field_errors'][0].title(), 'Два Пароля Не Совпадают.')
+    def test_create_user_error_pass(self):
+        data = user_create_data.copy()
+        data['re_password'] = 'V97tn7M4ru'
+        request = self.client.post('/api/v1/auth/users/', data, format='json')
+        self.assertEqual(request.status_code, 400)
+        self.assertEqual(request.data['non_field_errors'][0].title(), 'Два Пароля Не Совпадают.')
 
     def test_create_user_required(self):
         error_msg = 'Обязательное Поле.'
@@ -76,13 +76,13 @@ class ProfileRegTests(APITestCase):
         self.assertEqual(response.data['password'][0].title(), error_msg)
         self.assertEqual(response.data['re_password'][0].title(), error_msg)
 
-    # def test_create_user_unique_name(self):
-    #     self.client.post('/api/v1/auth/users/', user_create_data, format='json')
-    #     response = self.client.post('/api/v1/auth/users/', user_create_data, format='json')
-    #     self.assertEqual(
-    #         response.data['username'][0].title(),
-    #         'Пользователь С Таким Именем Уже Существует.'
-    #     )
+    def test_create_user_unique_name(self):
+        self.client.post('/api/v1/auth/users/', user_create_data, format='json')
+        response = self.client.post('/api/v1/auth/users/', user_create_data, format='json')
+        self.assertEqual(
+            response.data['username'][0].title(),
+            'Пользователь С Таким Именем Уже Существует.'
+        )
 
     def test_create_user_unique_email(self):
         self.client.post('/api/v1/auth/users/', user_create_data, format='json')
@@ -100,55 +100,55 @@ class ProfileRegTests(APITestCase):
         self.assertEqual(response.status_code, 400)
 
 
-# class ProfileAuthTests(APITestCase):
-#     def setUp(self):
-#         self.client.post('/api/v1/auth/users/', user_create_data, format='json')
-#         url = "/api/v1/auth/token/login/"
-#         user = {
-#             'username': user_create_data['username'],
-#             'password': user_create_data['password']
-#         }
-#         response = self.client.post(url, user, format='json')
-#         # self.assertEqual(response.status_code, 200)
-#         # self.assertTrue('auth_token' in response.data)
-#
-#     def test_user_pub_profile(self):
-#         user = FatUser.objects.get(username='anton')
-#         url = reverse("user-pub", kwargs={"pk": user.pk})
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, 200)
-#         self.assertFalse('email' in response.data)
-#
-#     def test_user_profile(self):
-#         user = FatUser.objects.get(username='anton')
-#         url = reverse("user")
-#         self.client.credentials(HTTP_AUTHORIZATION=f'Token {user.auth_token}')
-#         response = self.client.get(url)
-#
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTrue('email' in response.data)
+class ProfileAuthTests(APITestCase):
+    def setUp(self):
+        self.client.post('/api/v1/auth/users/', user_create_data, format='json')
+        url = "/api/v1/auth/token/login/"
+        user = {
+            'username': user_create_data['username'],
+            'password': user_create_data['password']
+        }
+        response = self.client.post(url, user, format='json')
+        # self.assertEqual(response.status_code, 200)
+        # self.assertTrue('auth_token' in response.data)
+
+    def test_user_pub_profile(self):
+        user = FatUser.objects.get(username='anton')
+        url = reverse("user-pub", kwargs={"pk": user.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse('email' in response.data)
+
+    def test_user_profile(self):
+        user = FatUser.objects.get(username='anton')
+        url = reverse("user")
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {user.auth_token}')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('email' in response.data)
 
 
-# class TestSocial(APITestCase):
-#     @classmethod
-#     def setUpClass(cls):
-#         super().setUpClass()
-#
-#         Social.objects.create(title='Social 1')
-#         Social.objects.create(title='Social 2')
-#
-#     def test_social_list(self):
-#         url = reverse("social-list")
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(len(response.json().get("results")), 2)
-#
-#     def test_social_detail(self):
-#         social = Social.objects.get(title='Social 1')
-#         url = reverse("social-detail", kwargs={"pk": social.pk})
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response.json().get("title"), 'Social 1')
+class TestSocial(APITestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        Social.objects.create(title='Social 1')
+        Social.objects.create(title='Social 2')
+
+    def test_social_list(self):
+        url = reverse("social-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json().get("results")), 2)
+
+    def test_social_detail(self):
+        social = Social.objects.get(title='Social 1')
+        url = reverse("social-detail", kwargs={"pk": social.pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json().get("title"), 'Social 1')
 
 
 class FatUserProfileTest(APITestCase):
@@ -161,18 +161,18 @@ class FatUserProfileTest(APITestCase):
         )
 
 
-# class ProfileAuthTests(APITestCase):
-#     def setUp(self):
-#         self.client.post('/api/v1/auth/users/', user_create_data, format='json')
-#         url = "/api/v1/auth/token/login/"
-#         user = {
-#             'username': user_create_data['username'],
-#             'password': user_create_data['password']
-#         }
-#         request = self.client.post(url, user, format='json')
-#
-#         self.assertEqual(request.status_code, 200)
-#         self.assertTrue('auth_token' in request.data)
+class ProfileAuthTests(APITestCase):
+    def setUp(self):
+        self.client.post('/api/v1/auth/users/', user_create_data, format='json')
+        url = "/api/v1/auth/token/login/"
+        user = {
+            'username': user_create_data['username'],
+            'password': user_create_data['password']
+        }
+        request = self.client.post(url, user, format='json')
+
+        self.assertEqual(request.status_code, 200)
+        self.assertTrue('auth_token' in request.data)
 
     def test_user_pub_profile(self):
         user = FatUser.objects.get(username='anton')
@@ -388,15 +388,15 @@ class FatUserProfileTest(APITestCase):
         response = self.client.get(reverse('user_me'))
         self.assertEqual(response.status_code, 401)
 
-    # def test_user_me_update(self):
-    #     self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_test1_token.key)
-    #     data = {
-    #         'middle_name': 'test',
-    #         'email': 'test2@mail.ru'
-    #     }
-    #     response = self.client.put(reverse('user_me'), data=data, format='json')
-    #     self.assertEqual(len(response.data), 3)
-    #     self.assertEqual(response.status_code, 200)
+    def test_user_me_update(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_test1_token.key)
+        data = {
+            'middle_name': 'test',
+            'email': 'test2@mail.ru'
+        }
+        response = self.client.put(reverse('user_me'), data=data, format='json')
+        self.assertEqual(len(response.data), 3)
+        self.assertEqual(response.status_code, 200)
 
     def test_user_update_me_invalid_email(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.user_test1_token.key)
